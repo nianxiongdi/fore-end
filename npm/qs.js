84b051a097fe38cb39acfa1b51c033bd7e15038e
nianxiongdi <41629087+nianxiongdi@users.noreply.github.com>
2019-10-14 14:04:43 +0800

    Update qs.js

--- npm/qs.js
+++ npm/qs.js
@@ -1,6 +1,4 @@
-```js
-
-
+'''js
 // qs.parse() - 将URL解析成对象的形式
 
 const qs = require('qs');
@@ -21,4 +19,4 @@ console.log(qs.stringify(obj));
  
 
 // 参考 https://blog.csdn.net/suwu150/article/details/78333452
-```
+'''
